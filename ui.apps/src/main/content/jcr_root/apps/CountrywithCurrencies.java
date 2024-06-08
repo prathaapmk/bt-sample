002949d2afc59a5c00cae0d1196d3c22fa251cc6
vamshi <bvamshikrishna390email.com>
2024-06-08 18:31:50 +0530

    added sling mutli and mutlicompositefiledmodel

---
+++ ui.apps/src/main/content/jcr_root/apps/CountrywithCurrencies.java
@@ -0,0 +1,28 @@
+package apps;
+
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = Resource.class)
+public class CountrywithCurrencies {
+	
+	@ValueMapValue
+	private String country;
+	
+	@ValueMapValue
+	private String currency;
+
+	public String getCountry() {
+		return country;
+	}
+
+	public String getCurrency() {
+		return currency;
+	}
+	
+	
+	
+
+}
